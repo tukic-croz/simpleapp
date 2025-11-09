5382332b5baa4333824069a43e433745c71241e1
Tomislav Ukić <tukic@croz.net>
2025-11-09 13:19:28 +0100

    POOL simple-lab9.apps-crc.testing

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <html> 
 	<head> 
-	<title>PHP Test</title>
+	<title>Updated PHP Test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+	<h1>PHP Updated</h1>
+		<p><b>An Example of Updated PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
-        <h2>LETS GO SWIM IN THE OCEAN</h2>
+        <h2>LETS GO SWIM IN THE POOL</h2>
 	</body> 
 </html>
